810cb86d59ff94865a61a73b9da143cca548b48f
Ngo Huynh Cong <congngo318@gmail.com>
2022-07-24 20:19:56 +0700

    update

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById("btnTinhTien").onclick = function(){
+    var name = document.getElementById("name").value;
+    var soKw = document.getElementById("soKw").value * 1;
+
+    
+}
